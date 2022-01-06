0070561c642969a81ff7a24cd9fd859609f74711
Sachin Shukla <91952010+Sachin4219@users.noreply.github.com>
2022-01-06 20:23:15 +0530

    Updated the difference between hard and easy
    Mwa ha ha

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 	
-	var c1,c2,c3,i;
-    var colors=[];
-    var div;
-    var col=Math.floor(Math.random()*6);
-    var container=document.getElementById('container');
-	var winmessage = document.querySelector(".winning-message");
+var c1,c2,c3,i,elements;
+var colors=[];
+var div;
+var col=Math.floor(Math.random()*6);
+var container=document.getElementById('container');
+var winmessage = document.querySelector(".winning-message");
 
 function win(){
 
-			for(i=0;i<6;i++)
+			for(i=0;i<elements;i++)
 			{
 				var element = document.querySelectorAll('#container div')[i];
 				element.style.visibility="visible";
@@ -17,7 +17,7 @@ function win(){
 		winmessage.innerText="Congrats You guessed it right";
 	}
 
-	for(i=0;i<6;i++){
+	for(i=0;i<elements;i++){
 			div = document.querySelectorAll('#container div')[i];
 			div.classList.add('el');
 			div.addEventListener("click",function(){
@@ -32,7 +32,13 @@ function win(){
 		}
 	function start(){
 		winmessage.innerText="";
-		for(i=0;i<6;i++)
+		var lev=document.getElementById('level').value;
+		if(lev==="Hard")
+			elements = 6;
+		else
+			elements = 3;
+		
+		for(i=0;i<elements;i++)
 		{
 			div = document.querySelectorAll('#container div')[i];
 			div.style.visibility="visible";
@@ -40,27 +46,25 @@ function win(){
 		document.querySelector('#header-2 #color').innerText="";
 		colors=[];
 
-		for(i=0;i<6;i++)
+		for(i=0;i<elememts;i++)
 		{
 			var element = document.querySelectorAll('#container div')[i];
-			var lev=document.getElementById('level').value;
-
+			
 			if(lev==="Hard"){
-
 				c1=(Math.floor(Math.random()*120) + Math.floor(Math.random()*80)).toString();
 				c2=(Math.floor(Math.random()*120) + Math.floor(Math.random()*80)).toString();
 				c3=(Math.floor(Math.random()*120) + Math.floor(Math.random()*80)).toString();
-
 			}
 				
 			else{
 			c1=Math.floor(Math.random()*256).toString();
 			c2=Math.floor(Math.random()*256).toString();
-			c3=Math.floor(Math.random()*256).toString(); }
+			c3=Math.floor(Math.random()*256).toString();
+			}
 
 			colors.push("rgb("+c1+", "+c2+", "+c3+")");
 			element.style.background = "rgb("+c1+","+c2+","+c3+")";
 		}	
-        col=Math.floor(Math.random()*6);
+        col=Math.floor(Math.random()*elements);
 		document.querySelector("#header-2 #color").innerText=colors[col];
 }
